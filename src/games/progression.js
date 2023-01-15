4cf7712d59623e7e8c622384aba05f6c3b5407c0
viktordiag <viktordiag@gmail.com>
2023-01-15 19:25:29 +0300

    add brain-progression

---
+++ src/games/progression.js
@@ -0,0 +1,8 @@
+import _ from 'lodash';
+import {greeting} from '../index.js';
+import {mainLogic} from '../index.js';
+export const gameProgression = () => {
+  const theTask = 'What number is missing in the progression?';
+  greeting(theTask);
+  };
+
