572ad4170e177a59f9a752549ecb0ab643a5646a
viktordiag <viktordiag@gmail.com>
2023-01-16 14:58:31 +0300

    hate

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -7,9 +7,9 @@
    greeting(theTask);
 
    for (let i = 1; i <= 3; i += 1) {
-     let numberOne = _.random(1,20);
-     let numberTwo = _.random(1,20);
-     let question  = `${numberOne} 44`;
+     let numberOne = 45 //_.random(1,20);
+     let numberTwo = 45 //_.random(1,20);
+     let question  = `${numberOne} ${numberTwo}`;
      let correctAnswer = gcd(numberOne, numberTwo);
      let uncorrect =  mainLogic(question, correctAnswer, i);
      if (uncorrect === 1) {
