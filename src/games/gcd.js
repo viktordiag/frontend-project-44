c2a14a342a967cbc722f31ee5f852bf3ab4dcc48
viktordiag <viktordiag@gmail.com>
2023-01-22 10:28:34 +0300

    fix gcd
    убрал решение через подключаемую библиотеку mathjs написал
    функцию gcd()

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -6,9 +6,24 @@
    const theTask = 'Find the greatest common divisor of given numbers.';
    greeting(theTask);
 
+   const gcd = (numberOne, numberTwo) => {
+     if(numberOne === numberTwo) {
+       return numberOne;
+       }
+     while(numberOne != numberTwo) {
+       if(numberOne < numberTwo) {
+	 numberTwo -= numberOne;
+       } else {
+	       numberOne -= numberTwo
+         }
+     }
+       return numberOne;
+   };
+       
+
    for (let i = 1; i <= 3; i += 1) {
-     let numberOne = 4 //_.random(1,20);
-     let numberTwo = 4 //_.random(1,20);
+     let numberOne = _.random(1,20);
+     let numberTwo = _.random(1,20);
      let question  = `${numberOne} ${numberTwo}`;
      let correctAnswer = gcd(numberOne, numberTwo);
      let uncorrect =  mainLogic(question, correctAnswer, i);
